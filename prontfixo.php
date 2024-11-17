8134bd28dc39c85ff0978f6e843be3dba58d422d
Yasmin Matos <yas.lm@hotmail.com>
2024-11-17 19:24:21 -0300

    Update prontfixo.php

--- prontfixo.php
+++ prontfixo.php
@@ -1,25 +1,33 @@
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <title>Prontuário do Paciente</title>
     <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f2f7fc;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-        }
 
-        .container {
-            background-color: #ffffff;
-            max-width: 600px;
-            width: 100%;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        }
+        
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f2f7fc;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh; /* Alterado para min-height */
+    margin: 0;
+    overflow-y: auto; /* Permite rolagem em caso de excesso de conteúdo */
+    box-sizing: border-box;
+}
+
+.container {
+    background-color: #ffffff;
+    max-width: 600px;
+    width: 100%;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    margin: 20px; /* Adicionado para evitar que o conteúdo toque nas bordas */
+}
+
 
         h1 {
             text-align: center;
